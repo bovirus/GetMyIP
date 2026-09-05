a20fa0a3f0d1deba1a8d2d9310b14314a49ffd12
Tim Kennedy <timthreetwelve@outlook.com>
2026-09-05 17:33:56 -0500

    Fix default value for auto refresh interval

--- GetMyIP/Configuration/UserSettings.cs
+++ GetMyIP/Configuration/UserSettings.cs
@@ -21,8 +21,11 @@ public partial class UserSettings : ConfigManager<UserSettings>
     [ObservableProperty]
     private bool _autoRefresh;
 
+    /// <summary>
+    /// The interval, in seconds, for periodic refresh.
+    /// </summary>
     [ObservableProperty]
-    private int _autoRefreshSeconds = TimeSpan.FromHours(1).Seconds;
+    private int _autoRefreshSeconds = 3600;
 
     /// <summary>
     ///  Used to determine if Debug level messages are included in the application log.
